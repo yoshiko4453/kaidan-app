9fed269d54f96ccbd2966f4f694be42a7e1839f9
yoshiko4453 <kaneko4453@gmail.com>
2026-04-16 19:21:26 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'kaidan-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const FILES = ['/kaidan-app/', '/kaidan-app/index.html', '/kaidan-app/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
